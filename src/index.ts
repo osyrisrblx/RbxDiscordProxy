a1897c5e563e5311736aad80595529dab81031dc
osyrisrblx <osyrisrblx@gmail.com>
2018-04-06 15:19:55 -0400

    Added placeId logging

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,23 @@ const DISCORD_PREFIX = "x-ratelimit-";
 const DISCORD_LIMIT = DISCORD_PREFIX + "limit";
 const DISCORD_REMAINING = DISCORD_PREFIX + "remaining";
 const DISCORD_RESET = DISCORD_PREFIX + "reset";
+
 const BANNED_FILE_PATH = "/banned.json";
+const BANNED_USERNAME = "Error";
+const BANNED_AVATAR_URL = "https://i.imgur.com/zjyzJsb.png";
 const BANNED_NOTIFICATION_TEXT = `This webhook has been banned from the \`osyr.is\` Discord proxy server for violating Discord rate limits too often!
 Please create a new webhook and change your code to reduce how many requests you send.
 Contact \`Osyris#0001\` for further help if needed.`;
+const BANNED_JSON = JSON.stringify({
+	username: BANNED_USERNAME,
+	avatar_url: BANNED_AVATAR_URL,
+	content: BANNED_NOTIFICATION_TEXT
+});
 
-let rates: {
+let data: {
 	[key: string]: {
 		name?: string;
+		placeId?: string;
 		token: string;
 		limit: number;
 		remaining: number;
@@ -30,10 +39,10 @@ let rates: {
 
 let bannedHookIds: string[] = [];
 
-function getRateData(hookId: string, hookToken: string) {
-	let rateData = rates[hookId];
-	if (!rateData) {
-		rateData = {
+function getHookData(hookId: string, hookToken: string) {
+	let hookData = data[hookId];
+	if (!hookData) {
+		hookData = {
 			token: hookToken,
 			limit: 0,
 			remaining: 1,
@@ -41,15 +50,15 @@ function getRateData(hookId: string, hookToken: string) {
 			errors: 0,
 			queue: []
 		};
-		rates[hookId] = rateData;
+		data[hookId] = hookData;
 	}
-	return rateData;
+	return hookData;
 }
 
 async function sendRequest(hookId: string, hookToken: string, payload: string) {
-	let rateData = getRateData(hookId, hookToken);
-	if (rateData.remaining > 0) {
-		rateData.remaining--;
+	let hookData = getHookData(hookId, hookToken);
+	if (hookData.remaining > 0) {
+		hookData.remaining--;
 		request(format(WEBHOOK_TEMPLATE, hookId, hookToken), {
 			method: "post",
 			headers: {
@@ -61,36 +70,36 @@ async function sendRequest(hookId: string, hookToken: string, payload: string) {
 			let limit = Number(res.headers[DISCORD_LIMIT]);
 			let remaining = Number(res.headers[DISCORD_REMAINING]);
 			if (!isNaN(reset)) {
-				rateData.reset = reset;
+				hookData.reset = reset;
 			}
 			if (!isNaN(limit)) {
-				rateData.limit = limit;
+				hookData.limit = limit;
 			}
 			if (!isNaN(remaining)) {
-				rateData.remaining = remaining;
+				hookData.remaining = remaining;
 			}
 			if (res.statusCode == 429) {
-				rateData.queue.push(payload);
+				hookData.queue.push(payload);
 			}
 		});
 	} else {
-		rateData.queue.push(payload);
+		hookData.queue.push(payload);
 	}
 }
 
 // process queue
 setInterval(() => {
 	let time = Math.floor(Date.now() / 1000);
-	let keys = Object.keys(rates);
+	let keys = Object.keys(data);
 	for (let i = 0; i < keys.length; i++) {
 		let key = keys[i];
-		let rateData = rates[key];
-		if (rateData.reset !== -1 && time >= rateData.reset) {
-			rateData.remaining = rateData.limit;
-			rateData.reset = -1;
-			for (let i = rateData.queue.length - 1; i >= 0; i--) {
-				if (rateData.remaining > 0) {
-					sendRequest(key, rateData.token, rateData.queue.splice(i, 1)[0]);
+		let hookData = data[key];
+		if (hookData.reset !== -1 && time >= hookData.reset) {
+			hookData.remaining = hookData.limit;
+			hookData.reset = -1;
+			for (let i = hookData.queue.length - 1; i >= 0; i--) {
+				if (hookData.remaining > 0) {
+					sendRequest(key, hookData.token, hookData.queue.splice(i, 1)[0]);
 				}
 			}
 		}
@@ -105,22 +114,20 @@ app.post("/api/webhooks/:hookId/:hookToken", (req, res) => {
 	let hookId = req.params.hookId;
 	let hookToken = req.params.hookToken;
 	if (bannedHookIds.indexOf(hookId) == -1) {
-		let rateData = getRateData(hookId, hookToken);
-		if (rateData.queue.length >= MAX_QUEUE_SIZE) {
-			rateData.errors++;
-			if (rateData.errors >= MAX_ERRORS) {
+		let hookData = getHookData(hookId, hookToken);
+		if (!hookData.placeId) {
+			let placeId = req.headers["roblox-id"];
+			if (placeId && typeof placeId == "string") {
+				hookData.placeId = placeId;
+			}
+		}
+		if (hookData.queue.length >= MAX_QUEUE_SIZE) {
+			hookData.errors++;
+			if (hookData.errors >= MAX_ERRORS) {
 				bannedHookIds.push(hookId);
 				fs.writeFile(BANNED_FILE_PATH, JSON.stringify(bannedHookIds), "utf8", () => {});
-				rateData.queue = [];
-				sendRequest(
-					hookId,
-					hookToken,
-					JSON.stringify({
-						username: "Error",
-						avatar_url: "https://i.imgur.com/zjyzJsb.png",
-						content: BANNED_NOTIFICATION_TEXT
-					})
-				);
+				hookData.queue = [];
+				sendRequest(hookId, hookToken, BANNED_JSON);
 			}
 		} else {
 			sendRequest(hookId, hookToken, req.body);
@@ -141,6 +148,7 @@ function getBodyAsync(req: request.Request) {
 
 interface Info {
 	name?: string;
+	placeId?: string;
 	queueSize?: number;
 	errorCount?: number;
 }
@@ -150,24 +158,25 @@ app.get("/", async (req, res) => {
 		hooks: [] as Info[],
 		banned: bannedHookIds
 	};
-	let ids = Object.keys(rates);
+	let ids = Object.keys(data);
 	for (let i = 0; i < ids.length; i++) {
 		let hookId = ids[i];
-		let data = rates[hookId];
+		let hookData = data[hookId];
 		let info: Info = {};
-		if (data.queue.length > 0) {
-			info.queueSize = data.queue.length;
-		}
-		if (data.errors > 0) {
-			info.errorCount = data.errors;
-		}
-		if (!data.name) {
+		if (!hookData.name) {
 			try {
-				data.name = JSON.parse(await getBodyAsync(request(format(WEBHOOK_TEMPLATE, hookId, data.token)))).name;
+				hookData.name = JSON.parse(
+					await getBodyAsync(request(format(WEBHOOK_TEMPLATE, hookId, hookData.token)))
+				).name;
 			} catch (e) {}
 		}
-		if (data.name) {
-			info.name = data.name;
+		info.name = hookData.name;
+		info.placeId = hookData.placeId;
+		if (hookData.queue.length > 0) {
+			info.queueSize = hookData.queue.length;
+		}
+		if (hookData.errors > 0) {
+			info.errorCount = hookData.errors;
 		}
 		result.hooks.push(info);
 	}
